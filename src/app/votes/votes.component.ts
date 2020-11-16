a1298b03c1834a9978b61f9ee9d52fe8c2448b6d
DaniKard <kajothada@gmail.com>
2020-11-16 05:36:06 +0200

    add detalis and vote

--- src/app/votes/votes.component.ts
+++ src/app/votes/votes.component.ts
@@ -7,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VotesComponent implements OnInit {
 
+yesVotes=0
+noVotes=0
+highVotes=5
+upVotes(){
+  this.yesVotes=this.yesVotes+1
+  if (this.yesVotes>this.highVotes){
+    console.log('hhhhh')
+  }
+}
+downVotes(){
+  this.noVotes=this.noVotes+1
+}
   constructor() { }
 
   ngOnInit(): void {
